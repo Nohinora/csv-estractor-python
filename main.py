757ce1170562b438e1ce19381c9649ac3f484712
Nohinora <nohinora@proton.me>
2025-10-11 10:38:24 +0200

    Code refactoring

--- main.py
+++ main.py
@@ -3,65 +3,56 @@ import common_data
 import re
 import sys
 
+fieldnames = ["date", "ddt", "model", "matricola", "shop", "legacy","verified","no_mat_found","negative_model_number"]
 
-data_to_write = []
+def extract(dic_reader, SHOP, MAT):
+    data_to_write = []
 
-def extract(input_file, output_file, SHOP, MAT):
     no_mat_count = 0 # matricola da dare alle macchine senza
-    count = 0
+    num_of_mat = count_mat_columns(dic_reader)
 
-    with open(input_file,'r') as f:
-        dicReader = csv.DictReader(f)
-        num_of_mat = count_mat_columns(dicReader)
+    for line in dic_reader:
+        # Estrazione matricole del giorno
+        matricole = []
+        for i in range(num_of_mat):
+            matricola = line['matricola' + str(i+1)]
+            if matricola != '':
+                matricole.append(matricola)
 
-        for line in dicReader:
-            # Estrazione matricole del giorno
-            matricole = []
-            for i in range(num_of_mat):
-                matricola = line['matricola' + str(i+1)]
-                if matricola != '':
-                    matricole.append(matricola)
-
-            # Associazione macchine a matricole del giorno in base alle matricole descritte dalle espressioni
-            for model in common_data.models.keys():
-                if line[model] == '': continue # se ci sono zero modelli passo a quello successivo
-                
-                model_amount = int(line[model]) # numero di macchine corrispondenti a questo modello
-                negative_model_number = 0
-
-                if model_amount < 0:
-                    negative_model_number = 1
-                    model_amount *= -1
-
-                for i in range(model_amount): # inizio il cilclo per l'associazione matricola modello, itero per il numero di modelli
-                    for idx, matricola in enumerate(matricole): # itero per cercare una corrispondenza nel vettore delle matricole del giorno
-                        if re.search(common_data.models[model],matricola): # in caso di corrispondenza elimino la matricola dal vettore, diminuisco la conta del modello corrente e esco da questo ciclo poichè ho associato al modello corrente la sua matricola
-                            matricole.pop(idx)
-                            model_amount -= 1
-                            count += 1
-                            data_to_write.append(generate_entry(line, model, matricola, SHOP, 0, negative_model_number))
-                            break
-
-                # Handle macchine senza matricole
-                while model_amount > 0:
-                    model_amount -= 1
-                    no_mat_count += 1
-                    count += 1
-                    print(f"{line['date']}, {model}, {MAT}-{no_mat_count:03}")
-                    data_to_write.append(generate_entry(line, model, f"{MAT}-{no_mat_count:03}", SHOP, 1, negative_model_number))
+        # Associazione macchine a matricole del giorno in base alle matricole descritte dalle espressioni
+        for model in common_data.models.keys():
+            if line[model] == '': continue # se ci sono zero modelli passo a quello successivo
             
-            # matricole avanzate
-            if len(matricole) > 0:
-                for matricola in matricole:
-                    data_to_write.append(generate_entry(line, matricola_avanzata(matricola), matricola, SHOP, 0, negative_model_number))
+            model_amount = int(line[model]) # numero di macchine corrispondenti a questo modello
+            negative_model_number = 0
 
-    print(count) # per coffee energy: 372
+            if model_amount < 0:
+                negative_model_number = 1
+                model_amount *= -1
 
-    with open(output_file, "w", newline="") as f:
-        fieldnames = ["date", "ddt", "model", "matricola", "shop", "legacy","verified","no_mat_found","negative_model_number"]
-        writer = csv.DictWriter(f, fieldnames=fieldnames)
-        writer.writeheader()
-        writer.writerows(data_to_write)
+            for i in range(model_amount): # inizio il cilclo per l'associazione matricola modello, itero per il numero di modelli
+                for idx, matricola in enumerate(matricole): # itero per cercare una corrispondenza nel vettore delle matricole del giorno
+                    if re.search(common_data.models[model],matricola): # in caso di corrispondenza elimino la matricola dal vettore, diminuisco la conta del modello corrente e esco da questo ciclo poichè ho associato al modello corrente la sua matricola
+                        matricole.pop(idx)
+                        model_amount -= 1
+                        data_to_write.append(generate_entry(line, model, matricola, SHOP, 0, negative_model_number))
+                        break
+
+            # Handle macchine senza matricole
+            while model_amount > 0:
+                model_amount -= 1
+                no_mat_count += 1
+                print(f"{line['date']}, {model}, {MAT}-{no_mat_count:03}")
+                data_to_write.append(generate_entry(line, model, f"{MAT}-{no_mat_count:03}", SHOP, 1, negative_model_number))
+        
+        # matricole avanzate
+        if len(matricole) > 0:
+            for matricola in matricole:
+                data_to_write.append(generate_entry(line, matricola_avanzata(matricola), matricola, SHOP, 0, negative_model_number))
+    
+    return data_to_write
+
+    
 
 # FUNCTIONS
 
@@ -100,4 +91,12 @@ if __name__ == "__main__":
     output_file = str(sys.argv[2])
     shop_name = str(sys.argv[3])
     no_mat_prefix = str(sys.argv[4])
-    extract(input_file, output_file, shop_name, no_mat_prefix)
+
+    dic_reader = csv.DictReader(open(input_file, "r"))
+
+    data_to_write = extract(dic_reader, shop_name, no_mat_prefix)
+
+    with open(output_file, "w", newline="") as f:
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(data_to_write)
